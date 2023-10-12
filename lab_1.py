aac407fc4a4764cce3cb23e19df08c53ee5ad260
Kirill Trofimov <kaha2015-2016@mail.ru>
2023-10-12 09:54:45 +0400

    Pre-final version

--- lab_1.py
+++ lab_1.py
@@ -1,38 +1,4 @@
-import datetime
-import time
-import json
-import csv
-import requests
+from utils import main
 
-begin = time.time()
-
-date = datetime.date.today()
-file = open("dataset.csv", "w")
-fwriter = csv.writer(file, delimiter = ",", lineterminator="\r")
-
-while True:
-
-    year = str(date.year)
-    month = str(date.month) if date.month > 9 else "0" + str(date.month)
-    day = str(date.day) if date.day > 9 else "0" + str(date.day)
-
-    if date.weekday() == 0 or date.weekday() == 6:
-        date = date - datetime.timedelta(1)
-        continue
-
-    date = date - datetime.timedelta(1)
-    
-    url = "https://www.cbr-xml-daily.ru/archive/" + year + "/" + month + "/" + day + "/daily_json.js"
-
-    response = requests.get(url)
-    if response.status_code != 200:
-        continue
-
-    json_f = json.loads(response.text)
-
-    fwriter.writerow([f"{year}.{month}.{day}", json_f["Valute"]["USD"]["Value"]])
-
-    if date.year < 2014:
-        break
-
-file.close()
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,42 @@
+import datetime
+import json
+import csv
+import requests
+
+def get_date(date):
+    year = str(date.year)
+    month = str(date.month) if date.month > 9 else "0" + str(date.month)
+    day = str(date.day) if date.day > 9 else "0" + str(date.day)
+    return year + "/" + month + "/" + day
+
+def get_past_day(date):
+    return date - datetime.timedelta(1)
+
+def record_currency_rate(file_name = "dataset1", boundary_date = datetime.date(2023, 1, 1), valute = "USD"):
+    date = get_past_day(datetime.date.today())
+    file = open(f"{file_name}.csv", "w")
+    fwriter = csv.writer(file, delimiter = ",", lineterminator="\r")
+
+    while date >= boundary_date:
+
+        if date.weekday() == 0 or date.weekday() == 6:
+            date = get_past_day(date)
+            continue
+    
+        url = "https://www.cbr-xml-daily.ru/archive/" + get_date(date) + "/daily_json.js"
+
+        response = requests.get(url)
+        if response.status_code != 200:
+            date = get_past_day(date)
+            continue
+
+        json_f = json.loads(response.text)
+
+        fwriter.writerow([get_date(date), json_f["Valute"][valute]["Value"]])
+
+        date = get_past_day(date)
+
+    file.close()
+
+def main() -> None:
+    record_currency_rate()
